08b2668603a75887bd0a064b95e1ab4a9d3b130e
eisuke.nakamura.m706@dirbato.co.jp <EisukeNakamura()@Dirbato-1423x>
2024-04-24 10:33:21 +0900

    SpringBoot終わり

--- HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
+++ HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +20,11 @@ public class HelloSpringBootController {
         String now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm:ss"));
         
         return "現在時刻:" + now;
+    }
+    @GetMapping("/plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 + val2;
+        return "計算結果：" + res;
     }
 }
