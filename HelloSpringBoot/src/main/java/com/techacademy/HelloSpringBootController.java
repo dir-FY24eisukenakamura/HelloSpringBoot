49994cc18d794ad6cc65534ac82d30a8da83de78
eisuke.nakamura.m706@dirbato.co.jp <EisukeNakamura()@Dirbato-1423x>
2024-04-24 10:28:25 +0900

    現在時刻

--- HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
+++ HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
@@ -1,5 +1,8 @@
 package com.techacademy;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,13 @@ public class HelloSpringBootController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello SpringBoot!";
+        return "Hello SpringBootの世界へ!";
+    }
+    
+    @GetMapping("now")
+    public String dispTime() {
+        String now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm:ss"));
+        
+        return "現在時刻:" + now;
     }
 }
